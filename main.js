eb04eeb20201c5fd2c135e88e58dcd971665f442
Roberto144412 <161595657+Roberto144412@users.noreply.github.com>
2024-05-15 18:21:53 -0300

    s

--- main.js
+++ main.js
@@ -41,14 +41,16 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 
-
 function atualizaCronometro(){ 
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];  
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+    document.getElementById("seg1").textContent = calculaTempo(tempos[1])[3];
     for (let i=0; i<contadores.length;i++){
-        //contadores[i].textContent = calculaTempo(tempos[i]);   
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[1])[3];
+        console.log("seg"+1)
+        
     }
 }
 
